79193c4773edb8bdb160064012b2fb15f3283b2f
Администратор <abdusattar.kasymbekov@gmail.com>
2022-05-03 06:59:53 +0600

    Initial commit

---
+++ Assets/Scripts/AudioChanger.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using easyar;
+using UnityEngine;
+
+public class AudioChanger : MonoBehaviour
+{
+    private ImageTrackerFrameFilter _filter;
+
+    private Dictionary<string, AudioClip> _imageToAudio;
+
+    public AudioClip huTao;
+    public AudioClip inazuma;
+    public AudioSource current;
+
+    public void Start()
+    {
+        _imageToAudio = new Dictionary<string, AudioClip>();
+        _filter = gameObject.GetComponent<ImageTrackerFrameFilter>();
+        if (_filter != null)
+        {
+            Debug.Log("Success");
+        }
+
+        if (huTao != null)
+        {
+            _imageToAudio["Hu Tao"] = huTao;
+        }
+
+        if (inazuma != null)
+        {
+            _imageToAudio["Ayaka"] = inazuma;
+        }
+    }
+
+    public void Update()
+    {
+        var theme = _filter!.TargetControllers
+            .Where(target => target.IsTracked)
+            .Select(target => target.name)
+            .FirstOrDefault();
+        Debug.Log(theme);
+        if (theme != null)
+        {
+            Debug.Log(current.clip != _imageToAudio[theme]);
+            if (current.clip != _imageToAudio[theme])
+            {
+                current.clip = _imageToAudio[theme];
+                current.Play();
+            }
+        }
+        else
+        {
+            current.Pause();
+        }
+    }
+}
